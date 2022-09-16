2e1910e3c0100fdede9cbac50ea7d69572985a89
MisiekMikolaj <misiekmikolaj12@gmail.com>
2022-09-16 20:01:18 +0200

    ErrorWithMigration

---
+++ TyperLeague/TyperLeague.DataAccess/TyperLeagueStorageContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
+
+namespace TyperLeague.DataAccess
+{
+    public class TyperLeagueStorageContext : DbContext
+    {
+        public TyperLeagueStorageContext(DbContextOptions<TyperLeagueStorageContext> opt) : base(opt)
+        {
+        }
+
+        public DbSet<Match> Matches { get; set; }
+    }
+}
---
+++ TyperLeague/TyperLeague.DataAccess/TyperLeagueStorageContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TyperLeague.DataAccess
+{
+    public class TyperLeagueStorageContextFactory : IDesignTimeDbContextFactory<TyperLeagueStorageContext>
+    {
+        public TyperLeagueStorageContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<TyperLeagueStorageContext>();
+            optionsBuilder.UseSqlServer(@"Data Source=.\SQLEXPRESS;Initial Catalog=TyperLeagueStorage;Integrated Security=True");
+            return new TyperLeagueStorageContext(optionsBuilder.Options);
+        }
+    }
+}
