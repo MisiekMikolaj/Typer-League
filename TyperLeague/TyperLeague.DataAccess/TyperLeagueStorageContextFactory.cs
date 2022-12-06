5c32e6887f590b9be27fbbd33bb92592892d2728
MisiekMikolaj <102438726+MisiekMikolaj@users.noreply.github.com>
2022-12-06 23:20:22 +0100

    Update TyperLeagueStorageContextFactory.cs

--- TyperLeague/TyperLeague.DataAccess/TyperLeagueStorageContextFactory.cs
+++ TyperLeague/TyperLeague.DataAccess/TyperLeagueStorageContextFactory.cs
@@ -8,11 +8,8 @@ namespace TyperLeague.DataAccess
         public TyperLeagueStorageContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<TyperLeagueStorageContext>();
-            optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=TyperLeagueStorage;Integrated Security=True");
+            optionsBuilder.UseSqlServer("");
             return new TyperLeagueStorageContext(optionsBuilder.Options);
         }
     }
 }
-
-//Data Source=.\\SQLEXPRESS;Initial Catalog=TyperLeagueStorage;Integrated Security=True
-//"Server=tcp:typerleague.database.windows.net,1433;Initial Catalog=TyperLeagueStorage;Persist Security Info=False;User ID=miki;Password=misiek12@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
